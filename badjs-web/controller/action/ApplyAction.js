e80be03217e88e515c64912abd3e8b0796010004
jancatpan <jancatpan@tencent.com>
2019-06-14 19:25:44 +0800

    style: format

--- badjs-web/controller/action/ApplyAction.js
+++ badjs-web/controller/action/ApplyAction.js
@@ -4,14 +4,13 @@
  * @date : 2015-01-07
  */
 
-
 var log4js = require('log4js'),
     logger = log4js.getLogger(),
     _ = require('underscore'),
     crypto = require('crypto'),
     ApplyService = require('../../service/ApplyService'),
     UserService = require('../../service/UserService'),
-    isError = function (res, error) {
+    isError = function(res, error) {
         if (error) {
             res.json({
                 ret: 1,
@@ -27,16 +26,16 @@ var REG_REFERER = /^https?:\/\/[^\/]+\//i;
 
 var _process = function(value, key) {
     if (value.createTime) {
-        value.createTime = (new Date(value.createTime) - 0);
+        value.createTime = new Date(value.createTime) - 0;
     }
 
     if (value.passTime) {
-        value.passTime = (new Date(value.passTime) - 0);
+        value.passTime = new Date(value.passTime) - 0;
     }
 };
 
-var processData = function (data) {
-    if(data.length) {
+var processData = function(data) {
+    if (data.length) {
         _.each(data, _process);
     } else {
         _process(data);
@@ -44,45 +43,43 @@ var processData = function (data) {
     return data;
 };
 
-
 var applyAction = {
-
-    addApply: function (params, req, res) {
+    addApply: function(params, req, res) {
         // 必要信息为空，则报错
         var url = params.url;
         var url_no_match = !REG_DOMAIN_STAR.test(url) && !REG_REFERER.test(url);
         if (!params.name) {
             return res.json({
                 ret: 1002,
-                msg: "项目名不能为空，请重新填写"
+                msg: '项目名不能为空，请重新填写'
             });
         }
         if (url_no_match) {
             return res.json({
                 ret: 1002,
-                msg: "业务url有误，请重新填写"
+                msg: '业务url有误，请重新填写'
             });
         }
         if (!params.userName) {
             return res.json({
                 ret: 1002,
-                msg: "负责人不能为空，请重新填写"
+                msg: '负责人不能为空，请重新填写'
             });
         }
         var userService = new UserService();
-        userService.findOne({loginName: params.userName}, item => {
+        userService.findOne({ loginName: params.userName }, item => {
             if (item && item.loginName == params.userName) {
                 var apply = params;
 
                 var applyService = new ApplyService();
                 if (apply.id) {
-                    applyService.update(apply, function (err, items) {
+                    applyService.update(apply, function(err, items) {
                         if (isError(res, err)) {
                             return;
                         }
                         res.json({
                             ret: 0,
-                            msg: "success-add"
+                            msg: 'success-add'
                         });
                     });
                 } else {
@@ -90,18 +87,21 @@ var applyAction = {
                     apply.status = 0;
                     apply.createTime = new Date();
                     apply.codePath = params.codePath;
-                    apply.appkey = crypto.createHash("md5").update(new Date - 0 + "badjsappkey" + params.user.loginName).digest('hex');
+                    apply.appkey = crypto
+                        .createHash('md5')
+                        .update(new Date() - 0 + 'badjsappkey' + params.user.loginName)
+                        .digest('hex');
 
                     if (apply.limitpv == '') {
                         apply.limitpv = 0;
                     }
-                    applyService.add(apply, function (err, items) {
+                    applyService.add(apply, function(err, items) {
                         if (isError(res, err)) {
                             return;
                         }
                         res.json({
                             ret: 0,
-                            msg: "success-add"
+                            msg: 'success-add'
                         });
                     });
                 }
@@ -114,16 +114,16 @@ var applyAction = {
         });
     },
 
-    queryListByUser: function (params, req, res) {
+    queryListByUser: function(params, req, res) {
         var applyService = new ApplyService();
         if (params.user.role != 1) {
-            applyService.queryListByUser(params, function (err, items) {
+            applyService.queryListByUser(params, function(err, items) {
                 if (isError(res, err)) {
                     return;
                 }
                 res.json({
                     ret: 0,
-                    msg: "success",
+                    msg: 'success',
                     data: {
                         role: params.user.role,
                         item: processData(items)
@@ -131,13 +131,13 @@ var applyAction = {
                 });
             });
         } else {
-            applyService.queryListByAdmin(params, function (err, items) {
+            applyService.queryListByAdmin(params, function(err, items) {
                 if (isError(res, err)) {
                     return;
                 }
                 res.json({
                     ret: 0,
-                    msg: "success",
+                    msg: 'success',
                     data: {
                         role: params.user.role,
                         item: processData(items)
@@ -145,30 +145,29 @@ var applyAction = {
                 });
             });
         }
-
     },
-    queryListByAdmin: function (params, req, res) {
+    queryListByAdmin: function(params, req, res) {
         var applyService = new ApplyService();
         //不是管理员的话直接返回错误提示
         if (params.user.role != 1) {
             res.json({
                 ret: 1003,
-                msg: "权限不足"
+                msg: '权限不足'
             });
             return;
         }
-        applyService.queryListByAdmin(params, function (err, items) {
+        applyService.queryListByAdmin(params, function(err, items) {
             if (isError(res, err)) {
                 return;
             }
             res.json({
                 ret: 0,
-                msg: "success",
+                msg: 'success',
                 data: processData(items)
             });
         });
     },
-    queryListBySearch: function (params, req, res) {
+    queryListBySearch: function(params, req, res) {
         var applyService = new ApplyService();
 
         var searchParam = {};
@@ -181,13 +180,13 @@ var applyAction = {
             searchParam.status = params.statusType;
         }
 
-        applyService.queryListBySearch(searchParam, function (err, items) {
+        applyService.queryListBySearch(searchParam, function(err, items) {
             if (isError(res, err)) {
                 return;
             }
             res.json({
                 ret: 0,
-                msg: "success",
+                msg: 'success',
                 data: {
                     role: params.user.role,
                     item: processData(items)
@@ -197,48 +196,52 @@ var applyAction = {
     },
     queryByApplyId: function(params, req, res) {
         var applyService = new ApplyService();
-        applyService.queryById({
-            id: params.applyId
-        }, function (err, apply) {
-            if (err) {
-                res.json({
-                    ret: 3,
-                    msg: err
-                });
-            } else {
-                res.json({
-                    ret: 0,
-                    data: processData(apply)
-                });
+        applyService.queryById(
+            {
+                id: params.applyId
+            },
+            function(err, apply) {
+                if (err) {
+                    res.json({
+                        ret: 3,
+                        msg: err
+                    });
+                } else {
+                    res.json({
+                        ret: 0,
+                        data: processData(apply)
+                    });
+                }
             }
-        });
+        );
     },
-    get: function (params, cb) {
+    get: function(params, cb) {
         var applyService = new ApplyService();
-        applyService.queryById({
-            id: params.applyId
-        }, function (err, apply) {
-            cb(err, apply);
-        });
+        applyService.queryById(
+            {
+                id: params.applyId
+            },
+            function(err, apply) {
+                cb(err, apply);
+            }
+        );
     },
-    update: function (params, req, res) {
+    update: function(params, req, res) {
         var as = new ApplyService();
-        as.update(params, function () {
-
-        });
+        as.update(params, function() {});
     },
-    remove: function (params, req, res) {
+    remove: function(params, req, res) {
         var applyService = new ApplyService();
-        applyService.remove(params, function (err) {
+        applyService.remove(params, function(err) {
             if (err) {
                 res.json({
                     ret: 3,
-                    msg: "fail remove"
+                    msg: 'fail remove'
                 });
             } else {
                 res.json({
                     ret: 0,
-                    msg: "success remove"
+                    msg: 'success remove'
                 });
             }
         });
