093a2f0af72b865644d619d680148c74e2a227b1
tickli <tickli@tencent.com>
2019-06-25 11:45:26 +0800

    fix: 修复微信通知排序错误的问题

--- badjs-web/service/WechatService.js
+++ badjs-web/service/WechatService.js
@@ -9,10 +9,10 @@ const mysqlUrl = pjConfig.mysql.url;
 
 var mdb = orm.connect(mysqlUrl, function (err, db) {
 
-    const yesterday = moment().subtract(2, 'day').format('YYYYMMDD') - 0;
+    const yesterday = moment().subtract(1, 'day').format('YYYYMMDD') - 0;
     const url = `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${pjConfig.wechat}`;
 
-    const sql = 'select s.*, a.userName, a.name from b_quality as s, b_apply as a where s.badjsid=a.id and a.status=1 and a.online=2 and s.pv>a.limitpv and s.rate > 0.5 and s.date>' + yesterday + ' order by s.rate desc;';
+    const sql = 'select s.*, a.userName, a.name from b_quality as s, b_apply as a where s.badjsid=a.id and a.status=1 and a.online=2 and s.pv>a.limitpv and s.rate > 0.5 and s.date=' + yesterday + ' order by s.rate + 0 desc;';
     db.driver.execQuery(sql, (err, data) => {
         mdb.close();
 
