c1a847140c3633ca85c41d1e669a8bbe2a72d6d8
tickli <tickli@tencent.com>
2019-09-19 01:10:42 +0800

    feat: rm useless codes

--- aegis-web/utils/email_tof.js
+++ aegis-web/utils/email_tof.js
@@ -2,12 +2,6 @@ const axios = require('axios');
 const log4js = require('log4js');
 const logger = log4js.getLogger();
 
-if (argv.indexOf('--project') >= 0) {
-    global.pjconfig = require(path.join(__dirname, "..", '/project.debug.json'));
-} else {
-    global.pjconfig = require(path.join(__dirname, "..", 'project.json'));
-}
-
 module.exports = function ({userList=['tickli'], title, subtitle, content, msgInfo}) {
     axios.post(global.pjconfig.tof, {
         'rtx': 1,
