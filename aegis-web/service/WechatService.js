10f2ebe64a9da4188d95a993719f31995f26502f
tickli <tickli@tencent.com>
2019-12-07 00:08:52 +0800

    feat: 暂时限制微信扣分通知项目总数

--- aegis-web/service/WechatService.js
+++ aegis-web/service/WechatService.js
@@ -12,7 +12,7 @@ var mdb = orm.connect(mysqlUrl, function (err, db) {
     const yesterday = moment().subtract(1, 'day').format('YYYYMMDD') - 0;
     const url = `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${pjConfig.wechat}`;
 
-    const sql = 'select s.*, a.userName, a.name from b_quality as s, b_apply as a where s.badjsid=a.id and a.status=1 and a.online=2 and s.pv>a.limitpv and s.rate > 0.5 and s.date=' + yesterday + ' order by s.rate + 0 desc;';
+    const sql = 'select s.*, a.userName, a.name from b_quality as s, b_apply as a where s.badjsid=a.id and a.status=1 and a.online=2 and s.pv>a.limitpv and s.rate > 0.5 and s.date=' + yesterday + ' order by s.rate + 0 desc limit 25;';
     db.driver.execQuery(sql, (err, data) => {
         mdb.close();
 
